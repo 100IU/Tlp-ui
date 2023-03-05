55f50018b4f853cd17ca55596930e509a9ddfe52
admin <2790238326@qq.com>
2023-03-05 09:10:36 +0000

    update src/main.js.
    Signed-off-by: admin <2790238326@qq.com>

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ShowCode from './components/ShowCode'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 import tlpmentui from 'tlpment-ui'
 import 'tlpment-ui/dist/tlp-ui.css'
-Vue.use(ElementUI);
+
 Vue.use(tlpmentui)
 
 Vue.component('ShowCode', ShowCode)
